434e2b1a8640d2823303de76db5295f0a27f72b2
olakayCoder1 <programmerolakay@gmail.com>
2022-11-10 09:04:54 +0100

    initial

--- bus/settings.py
+++ bus/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"] 
 
 
 # Application definition
@@ -134,8 +134,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL='media/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS  = [
+    (BASE_DIR / "static"),
+]
+
+
+MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
 # Default primary key field type
--- bus/urls.py
+++ bus/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home',include('schools.urls')),
     path('api/v1/',include('api.urls')),
-    path('payment/',include('subscriptions.urls')),
+    # path('payment/',include('subscriptions.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
