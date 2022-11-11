3e8fb20033a997b8b13aff7722ccaca5ec406a07
olakayCoder1 <programmerolakay@gmail.com>
2022-11-11 14:34:42 +0100

    staticfiles

--- bus/urls.py
+++ bus/urls.py
@@ -18,6 +18,8 @@ from django.urls import path , include , re_path
 from drf_yasg.views import get_schema_view 
 from drf_yasg import openapi
 from rest_framework import permissions 
+from django.conf import settings
+from django.conf.urls.static import  static
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -41,4 +43,7 @@ urlpatterns = [
     path('payment/',include('subscriptions.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
