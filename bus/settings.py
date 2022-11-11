fa2068d0267534f55a7f9bf25512eb8c284a194c
olakayCoder1 <programmerolakay@gmail.com>
2022-11-11 15:16:18 +0100

    database connection

--- bus/settings.py
+++ bus/settings.py
@@ -150,11 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
-STATICFILES_DIRS  = [
-    (BASE_DIR / "static"),
-]
+STATICFILES_DIRS  = [   (BASE_DIR / "static"),]
 django_heroku.settings(locals())
 
 MEDIA_URL='/media/'
