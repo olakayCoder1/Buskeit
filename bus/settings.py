126f43df5bc302022b5a390241a75da73c35a61f
olakayCoder1 <programmerolakay@gmail.com>
2022-11-14 03:49:05 +0100

    image display fixed

--- bus/settings.py
+++ bus/settings.py
@@ -149,15 +149,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
+STATIC_URL = "static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STATICFILES_DIRS  = [   (BASE_DIR / "static") ]
 django_heroku.settings(locals())
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- bus/urls.py
+++ bus/urls.py
@@ -20,6 +20,7 @@ from drf_yasg import openapi
 from rest_framework import permissions 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 
 schema_view = get_schema_view(
@@ -42,6 +43,7 @@ urlpatterns = [
     path('payment/',include('subscriptions.urls')),
     path('home',include('schools.urls')),
     path('api/v1/',include('api.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve , {'document_root':settings.MEDIA_ROOT}),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
