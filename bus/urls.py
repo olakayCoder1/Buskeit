9816093c822da081581da2c2e691b0c6ea5e56d1
olakayCoder1 <programmerolakay@gmail.com>
2022-11-14 03:30:35 +0100

    image display fixed

--- bus/urls.py
+++ bus/urls.py
@@ -44,6 +44,6 @@ urlpatterns = [
     path('api/v1/',include('api.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
